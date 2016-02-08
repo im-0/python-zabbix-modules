7f8403772d107e1603e76d97128a84c5d9494152
Ivan Mironov <mironov.ivan@gmail.com>
2016-02-08 13:01:23 +0300

    Add special type for discovery items.

--- zabbix_module/types.py
+++ zabbix_module/types.py
@@ -1,3 +1,10 @@
+from __future__ import absolute_import
+
+import json
+
+import six
+
+
 class BaseStringValue(object):
     def __init__(self, format_string, *args, **kwargs):
         if (not args) and (not kwargs):
@@ -18,3 +25,25 @@ class Text(BaseStringValue):
 
 class NotSupported(BaseStringValue):
     pass
+
+
+class Discovery(object):
+    def __init__(self, macros_list=None):
+        if macros_list is None:
+            self._macros_list = []
+        else:
+            self._macros_list = list(macros_list)
+
+    def add_macros(self, macros_dict):
+        self._macros_list.append(macros_dict)
+
+    def get_string(self):
+        return json.dumps({
+            'data': list(
+                    dict(('{#%s}' % name, value)
+                         for name, value in six.iteritems(macros))
+                    for macros in self._macros_list)
+        })
+
+    def __str__(self):
+        return self.get_string()
